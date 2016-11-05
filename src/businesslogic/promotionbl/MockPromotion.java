13486a7e3072645ac18f5dd950bccaeebe338245
Loohaze <809739692@qq.com>
2016-11-05 10:43:55 +0800

    HY

--- src/businesslogic/promotionbl/MockPromotion.java
+++ src/businesslogic/promotionbl/MockPromotion.java
@@ -40,25 +40,20 @@ public class MockPromotion extends Promotion{
 	}
 	
 	public PromotionVO getPromotion(String name){
-		System.out.println("getPromotion success");
 		return null;
 	}
 	public PromotionVO[] getAllPromotion(){
-		System.out.println("getAllPromotion success");
 		return null;
 	}
 	
 	public ResultMessage addPromotion(PromotionVO vo){
-		System.out.println("addPromotion success");
 		return null;
 	}
 	
 	public ResultMessage modifyPromotion(PromotionVO vo){
-		System.out.println("modifyPromotion success");
 		return null;
 	}
 	public ResultMessage delPromotion(PromotionVO vo){
-		System.out.println("delPromotion success");
 		return null;
 	}
 
---
+++ src/businesslogictest/HotelTest.java
@@ -0,0 +1,13 @@
+package businesslogictest;
+
+import businesslogic.hotelbl.*;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HotelTest {
+
+	private MockHotel hotel;
+	
+	
+}
